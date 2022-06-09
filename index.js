c0e706994c5218f75aca00e31a741e67cab39166
Sondre Espeland <sondesp@stud.ntnu.no>
2022-06-09 03:02:14 +0200

    Added morgan to development pipeline

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+const morgan = require('morgan')
 const express = require('express')
 const app = express()
 const Joi = require('joi')
 
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+if (app.get('env') === 'development') {
+    app.use(morgan('tiny'))
+    console.log('Morgan enabled in development...')
+}
 
 let genreNames = ["horror", "comedy", "action", "drama", "romance", "thriller", "sci-fi", "fantasy", "mystery", "animation", "adventure", "crime", "documentary", "family", "history", "music", "war", "western"]
 let genres = []
