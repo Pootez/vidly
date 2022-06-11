ac13464ca640d4a6a12135d10616a54ac4c2e5c9
Sondre Espeland <sondesp@stud.ntnu.no>
2022-06-11 02:54:41 +0200

    Added persistence

--- routes/genres.js
+++ routes/genres.js
@@ -1,67 +1,124 @@
 const Joi = require('Joi')
 const express = require('express')
 const router = express.Router();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/vidly')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.log('Could not connect to MongoDB:', err))
+    .then(() => populateIfEmpty())
 
-let genreNames = ["horror", "comedy", "action", "drama", "romance", "thriller", "sci-fi", "fantasy", "mystery", "animation", "adventure", "crime", "documentary", "family", "history", "music", "war", "western"]
-let genres = []
-genreNames.forEach(obj => {
-    genres.push({
-        id: genres.length,
-        name: obj
+const Genre = mongoose.model('Genre', mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 50
+    }
+}))
+
+async function populateCollection() {
+    let genreNames = ["horror", "comedy", "action", "drama", "romance", "thriller", "sci-fi", "fantasy", "mystery", "animation", "adventure", "crime", "documentary", "family", "history", "music", "war", "western"]
+    let genres = []
+    genreNames.forEach(obj => {
+        genres.push(
+            new Genre({
+                name: obj
+            }))
     })
-})
+    try {
+        genres.forEach(genre => genre.save())
+        console.log('Number of genres:', genres.length)
+    }
+    catch (ex) {
+        console.log(ex.error)
+    }
+}
+
+async function isCollectionEmpty() {
+    console.log('Checking if collection is empty...')
+    try {
+        const count = await Genre.estimatedDocumentCount()
+        return count === 0
+    }
+    catch (ex) {
+        console.log(ex.error)
+    }
+}
+
+async function populateIfEmpty() {
+    try {
+        if (await isCollectionEmpty()) {
+            console.log('Populating collection...')
+            populateCollection()
+        }
+    }
+    catch (ex) {
+        console.log(ex.error)
+    }
+}
 
 router.get('/', (req, res) => {
-    res.send(genres)
+    Genre
+        .find({})
+        .select('name')
+        .then(genres => res.send(genres))
 })
 
 router.get('/:query', (req, res) => {
-    let genre = genres.find(obj => obj.id === parseInt(req.params.query))
-    if (!genre) genre = genres.find(obj => obj.name === req.params.query)
-
-    if (!genre) return res.status(404).send("No genre related to that query.")
-
-    res.send(genre)
+    const search = mongoose.Types.ObjectId.isValid(req.params.query)
+        ? { _id: req.params.query }
+        : { name: req.params.query }
+    Genre
+        .find(search)
+        .then(genres => {
+            if (genres.length === 0) return res.status(404).send("No genre related to that query.")
+            else {
+                res.send(genres[0])
+            }
+        })
 })
 
 router.post('/', (req, res) => {
     const { error } = validateGenre(req.body)
     if (error) return res.status(400).send(error.details[0].message)
 
-    if (genres.find(obj => obj === req.body.name)) return res.status(400).send('Genre already exists.')
-
-    const genre = {
-        id: genres.length,
+    const genre = new Genre({
         name: req.body.name
-    }
-    genres.push(genre)
-    res.send(genre)
+    })
+
+    Genre.find({ name: req.body.name }).count()
+        .then(count => {
+            if (count > 0) return res.status(400).send('Genre already exists.')
+
+            genre.save()
+            res.send(genre)
+        })
 })
 
 router.put('/', (req, res) => {
-    let target = genres.find(obj => obj.id === parseInt(req.body.id))
-    if (!target) return res.status(404).send("No genre with that id.")
+    Genre.findById(req.body.id).then(genre => {
+        if (!genre) return res.status(404).send("No genre with that id.")
 
-    const { error } = validateGenre({ name: req.body.name })
-    if (error) return res.status(400).send(error.details[0].message)
+        const { error } = validateGenre({ name: req.body.name })
+        if (error) return res.status(400).send(error.details[0].message)
 
-    const genre = {
-        id: req.body.id,
-        name: req.body.name
-    }
-    const index = genres.indexOf(target)
-    genres[index] = genre
-    res.send(genre)
+        Genre.find({ name: req.body.name }).count()
+            .then(count => {
+                if (count > 0) return res.status(400).send('Genre already exists.')
+
+                genre.name = req.body.name
+                genre.save().then(obj => res.send(obj))
+            })
+    })
 })
 
 router.delete('/', (req, res) => {
-    let target = genres.find(obj => obj.id === req.body.id)
-    if (!target) return res.status(404).send("No genre with that id.")
+    Genre.findByIdAndDelete(req.body.id).then(genre => {
+        if (!genre) return res.status(404).send("No genre with that id.")
 
-    const index = genres.indexOf(target)
-    genres.splice(index, 1)
-
-    res.send(target)
+        res.send(genre)
+    })
 })
 
 function validateGenre(genre) {
