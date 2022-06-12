9c4c4c43b8e44c6fd6b56d2fe6eed4f0042e70a6
Sondre Espeland <sondesp@stud.ntnu.no>
2022-06-13 01:44:15 +0200

    Movie model

---
+++ models/movie.js
@@ -0,0 +1,42 @@
+const { number } = require('Joi')
+const Joi = require('Joi')
+const mongoose = require('mongoose')
+
+const Movie = mongoose.model('Movie', mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 50
+    },
+    genre: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Genre'
+    },
+    numberInStock: {
+        type: Number,
+        default: 0,
+        required: true,
+        min: 0
+    },
+    dailyRentalRate: {
+        type: Number,
+        default: 0,
+        min: 0
+    }
+}))
+
+function validateMovie(genre) {
+    const schema = Joi.object({
+        title: Joi.string().min(3).required(),
+        genre: Joi.object().required(),
+        numberInStock: Joi.number().min(0).required(),
+        dailyRentalRate: number.min(0).required()
+    })
+
+    return schema.validate(genre)
+}
+
+exports.Movie = Movie
+exports.validateMovie = validateMovie
