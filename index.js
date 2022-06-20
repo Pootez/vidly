cdefe0894d8e119464bd7b4bbd8f6f12dc563581
Sondre Espeland <sondesp@stud.ntnu.no>
2022-06-20 16:50:01 +0200

    Implemented authentification with webtokens

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const morgan = require('morgan')
 const genres = require('./routes/genres')
 const movies = require('./routes/movies')
 const customers = require('./routes/customers')
 const rentals = require('./routes/rentals')
 const users = require('./routes/users')
+const auth = require('./routes/auth')
 const defaultRouter = require('./routes/default')
+
+const config = require('config')
+const morgan = require('morgan')
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+
+if (!config.get('jwtPrivateKey')) {
+    console.log('FATAL ERROR: jwtPrivateKey is not defined.')
+    process.exit(1)
+}
 
 mongoose.connect('mongodb://localhost/vidly')
     .then(() => console.log('Connected to MongoDB...'))
@@ -27,6 +35,7 @@ app.use('/api/movies', movies)
 app.use('/api/customers', customers)
 app.use('/api/rentals', rentals)
 app.use('/api/users', users)
+app.use('/api/auth', auth)
 app.use('', defaultRouter)
 
 const port = process.env.PORT || 3000
